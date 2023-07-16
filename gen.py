b4611571d09120fbc354a3339f5840a134932fc8
codeabuu <abdulngui718@gmail.com>
2023-07-16 16:22:29 -0700

    random strong password generator

---
+++ gen.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import random
+password = ""
+for chars in range(65, 91):#A TO Z uppercase characters from ascii
+    password += chr(chars)
+for chars in range(97, 123):
+    """a to z lowercase chars from ascii"""
+    password += chr(chars)
+
+symbols = ["!","@","#","$","%","^","&","*","_"]
+
+password = random.sample(password, 5)#sample password according to chars
+
+password += random.sample(symbols, 3)#sample symbols
+
+random.shuffle(password)#shuffles both chars and symbols
+
+password = "".join(password)#joins symbols with chars to generate password
+
+print(password)
